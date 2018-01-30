dc07b1666ce55e1c9ea8fd3b2439252b0ff57ee9
mughees605 <mughees605@gmail.com>
2018-01-31 01:52:34 +0500

    pure functio definition

--- 03-FUNCTION/FUNCTION_GROWING.js
+++ 03-FUNCTION/FUNCTION_GROWING.js
@@ -19,3 +19,11 @@ function printFarmInventory(cows, chickens, pigs){
 }
 
 printFarmInventory(3,4,12)
+
+// Pure Function
+// A pure function is a specific kind of value-producing function that not
+// only has no side effects but also doesn’t rely on side effects from other
+// code—for example, it doesn’t read global variables that are occasionally
+// changed by other code. A pure function has the pleasant property that,
+// when called with the same arguments, it always produces the same value
+// (and doesn’t do anything else).
