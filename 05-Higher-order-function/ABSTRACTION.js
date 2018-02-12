64a6e46e524e30b6aae3b62c968f62aa4fb770cd
mughees605 <mughees605@gmail.com>
2018-02-12 23:10:14 +0500

    abstracton example

--- 05-Higher-order-function/ABSTRACTION.js
+++ 05-Higher-order-function/ABSTRACTION.js
@@ -1,1 +1,20 @@
 // Abstractions hide details and give us the ability to talk about problems at a higher (or more abstract) level.
+
+
+function repeat(n, action) {
+    for (let i = 0; i < n; i++) {
+      action(i);
+    }
+  }
+  
+  //repeat(3, console.log);
+  // → 0
+  // → 1
+  // → 2
+
+  let message = "Loading"
+  repeat(5 , i =>{
+      message += "."
+  })
+  console.log(message) 
+  // Loading.....
