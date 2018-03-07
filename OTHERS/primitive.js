218c2474e9ada7c3cc3837d1e560f1b39d69073b
mughees605 <mughees605@gmail.com>
2018-03-07 12:59:28 +0500

    primitive data types added

---
+++ OTHERS/primitive.js
@@ -0,0 +1,8 @@
+// A primitive (primitive value, primitive data type) is data that is not an object and 
+// has no methods.
+//  In JavaScript, there are 6 primitive data types:
+//  string, number, boolean, null, undefined, symbol (new in ECMAScript 2015).
+
+// Most of the time, a primitive value is represented directly at the lowest level of the language implementation.
+
+// All primitives are immutable (cannot be changed).
