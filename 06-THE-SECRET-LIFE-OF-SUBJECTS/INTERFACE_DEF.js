de435176bf7ec5003c897175b25ca1391b2a5f23
mughees605 <mughees605@gmail.com>
2018-03-25 23:00:45 +0500

    added getter and setter file

---
+++ 06-THE-SECRET-LIFE-OF-SUBJECTS/INTERFACE_DEF.js
@@ -0,0 +1,4 @@
+// Interfaces
+// An interface is a description of the actions that an 
+// object can do... for example when you flip a light 
+// switch, the light goes on, you don't care how, just that it does. 
