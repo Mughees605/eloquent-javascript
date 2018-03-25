958a7cee93dad61b996a5f080db21a7d34e65123
mughees605 <mughees605@gmail.com>
2018-03-25 23:08:02 +0500

    setter definition

--- 06-THE-SECRET-LIFE-OF-SUBJECTS/GETTERS_SETTERS.js
+++ 06-THE-SECRET-LIFE-OF-SUBJECTS/GETTERS_SETTERS.js
@@ -1,3 +1,7 @@
 // GETTER DEFINITION
 
 //The get syntax binds an object property to a function that will be called when that property is looked up.
+
+//SETTER DEFINITION
+
+// The set syntax binds an object property to a function to be called when there is an attempt to set that property.
