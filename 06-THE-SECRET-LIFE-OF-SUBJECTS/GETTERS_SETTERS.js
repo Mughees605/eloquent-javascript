83ca8440bc6ad8c1ce87caadc5b50c8d37b1154d
mughees605 <mughees605@gmail.com>
2018-03-25 23:10:01 +0500

    added getters and setter example

--- 06-THE-SECRET-LIFE-OF-SUBJECTS/GETTERS_SETTERS.js
+++ 06-THE-SECRET-LIFE-OF-SUBJECTS/GETTERS_SETTERS.js
@@ -5,3 +5,29 @@
 //SETTER DEFINITION
 
 // The set syntax binds an object property to a function to be called when there is an attempt to set that property.
+
+
+// GETTERS AND SETTERS EXAMPLE
+
+class Temperature {
+    constructor(celsius) {
+        this.celsius = celsius;
+    }
+    get fahrenheit() {
+        return this.celsius * 1.8 + 32;
+    }
+    set fahrenheit(value) {
+        this.celsius = (value - 32) / 1.8;
+    }
+
+    static fromFahrenheit(value) {
+        return new Temperature((value - 32) / 1.8);
+    }
+}
+
+let temp = new Temperature(22);
+console.log(temp.fahrenheit);
+// → 71.6
+temp.fahrenheit = 86;
+console.log(temp.celsius);
+  // → 30
