3361c4f12e963fffd72dc69a1391d9d92c5638e1
mughees605 <mughees605@gmail.com>
2018-04-10 22:05:27 +0500

    another example

--- 06-THE-SECRET-LIFE-OF-SUBJECTS/CALL.js
+++ 06-THE-SECRET-LIFE-OF-SUBJECTS/CALL.js
@@ -3,3 +3,15 @@ function normalize() {
     }
     normalize.call({coords: [0, 2, 3], length: 5});
     // → [0, 0.4, 0.6]
+
+    function speak(line) {
+        console.log(`The ${this.type} rabbit says '${line}'`);
+        }
+        let whiteRabbit = {type: "white", speak};
+        let hungryRabbit = {type: "hungry", speak};
+        whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!");
+        // // → The white rabbit says 'Oh my ears and whiskers, how
+        // //
+        // late it's getting!'
+        hungryRabbit.speak("I could use a carrot right now.");
+        // → The hungry rabbit says 'I could use a carrot right now.'
