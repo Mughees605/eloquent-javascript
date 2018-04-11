66ed31434f98a633bc65ee745e1eba87fbfbb42d
mughees605 <mughees605@gmail.com>
2018-04-11 21:20:15 +0500

    example

--- 06-THE-SECRET-LIFE-OF-SUBJECTS/SYMBOLS.js
+++ 06-THE-SECRET-LIFE-OF-SUBJECTS/SYMBOLS.js
@@ -4,3 +4,6 @@ const toStringSymbol = Symbol("toString");
 Array.prototype[toStringSymbol] = function() {
 return `${this.length} cm of blue yarn`;
 };
+console.log([1, 2].toString());
+// → 1,2
+console.log([1, 2][toStringSymbol]()
