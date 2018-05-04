fa4f31d493537eeb1b82fb9846a8fb374795dfe6
mughees605 <mughees605@gmail.com>
2018-05-04 22:20:57 +0500

    create new state

--- 07-PROJECT-A-ROBOT/VILLAGE_STATE.js
+++ 07-PROJECT-A-ROBOT/VILLAGE_STATE.js
@@ -1,16 +1,23 @@
 import { roadGraph } from './BUILDING_GRAPH.js'
 
-class VillageState{
-    constructor(place, parcels){
-        this.place = place;
-        this.parcels = parcels;
-    }
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
 
-    move(destination){
-      // check whether there is a road going to this destination
-      if(!roadGraph[this.place].includes(destination)){
-        return this
-      }
-      //else it creates a new state
+  move(destination) {
+    // check whether there is a road going to this destination
+    if (!roadGraph[this.place].includes(destination)) {
+      return this
     }
+    //else it creates a new state
+    else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) return p;
+        return { place: destination, address: p.address };
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
 }
